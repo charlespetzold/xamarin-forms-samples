4084c439b69e13a0b978b10f4f360663c205117c
Charles Petzold <chape@microsoft.com>
2018-06-13 16:38:34 -0400

    Essentials samples updates

--- MonkeySee/MonkeySee/MonkeySee/MainPage.xaml.cs
+++ MonkeySee/MonkeySee/MonkeySee/MainPage.xaml.cs
@@ -8,70 +8,27 @@ namespace MonkeySee
 {
 	public partial class MainPage : ContentPage
 	{
-        UrhoSurface urhoSurface;
         MonkeyDo monkeyDo;
 
 		public MainPage()
 		{
 			InitializeComponent();
 
-            urhoSurface = new UrhoSurface();
-            Content = urhoSurface;
-
             OrientationSensor.ReadingChanged += (args) =>
             {
                 System.Numerics.Quaternion q = args.Reading.Orientation;
+
+                // Convert to Urho Quaternion, and swap Y and Z values to 
+                //  convert from right-hand to left-hand coordinates.
                 monkeyDo.Orientation = new Quaternion(q.X, q.Z, q.Y, q.W);
-
-
-                return;
-
-                Urho.Quaternion urhoQ = new Quaternion(q.X, q.Y, q.Z, q.W);
-
-                quaternion.Text = String.Format("{0:F3} {1:F3} {2:F3} {3:F3}", q.X, q.Y, q.Z, q.W);
-
-                q = System.Numerics.Quaternion.Normalize(q);
-
-                normalized.Text = String.Format("{0:F3} {1:F3} {2:F3} {3:F3}", q.X, q.Y, q.Z, q.W);
-
-                double a = 2 * Math.Acos(q.W);
-
-                float sine = (float)Math.Sin(a / 2);
-
-                System.Numerics.Vector3 v = new System.Numerics.Vector3(q.X * sine, q.Y * sine, q.Z * sine);
-
-                axis.Text = String.Format("{0:F2} {1:F2} {2:F2}", v.X, v.Y, v.Z);
-
-                angle.Text = (180 * a / Math.PI).ToString("F0");
-
-                v = System.Numerics.Vector3.Normalize(v);
-
-                normAxis.Text = String.Format("{0:F2} {1:F2} {2:F2}", v.X, v.Y, v.Z);
-
-                urhoQ.ToAxisAngle(out Vector3 axisUrho, out float angleUrho);
-
-                urhoAxis.Text = String.Format("{0:F2} {1:F2} {2:F2}", axisUrho.X, axisUrho.Y, axisUrho.Z);
-
-                urhoAngle.Text = angleUrho.ToString("F3");
-
-
-
-
-
             };
-
-
-
         }
 
         protected override async void OnAppearing()
         {
             base.OnAppearing();
 
-            monkeyDo = await urhoSurface.Show<MonkeyDo> (new ApplicationOptions(assetsFolder: "Data")
-            {
-                Orientation = ApplicationOptions.OrientationType.Portrait
-            });
+            monkeyDo = await urhoSurface.Show<MonkeyDo> (new ApplicationOptions(assetsFolder: "Data"));
 
             try
             {
@@ -87,8 +44,6 @@ namespace MonkeySee
                     Margin = new Thickness(50)
                 };
             }
-
-            
         }
 
         protected override void OnDisappearing()
--- MonkeySee/MonkeySee/MonkeySee/MonkeyDo.cs
+++ MonkeySee/MonkeySee/MonkeySee/MonkeyDo.cs
@@ -1,73 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Urho;
-using Urho.Actions;
+﻿using Urho;
 
 namespace MonkeySee
 {
     public class MonkeyDo : Application
     {
-        //      Node mainNode;
         Node cameraNode;
-
-        Matrix3x4 cameraTransform;
-/*
-        Matrix3x4 monkeyLookAt = CreateLookAt(new Vector3(0, 0, -10),  //eye
-                                              new Vector3(0, 3, 0),     // target (center of monkey)
-                                              new Vector3(0, 1, 0));    // up
-
-*/
-
-
+        Matrix4 cameraTransform;
+        Quaternion orientation = Quaternion.Identity;
 
         public MonkeyDo(ApplicationOptions options = null) : base(options)
         {
-
         }
-
-        Quaternion orientation = Quaternion.Identity;
 
         public Quaternion Orientation
         {
             set
             {
                 orientation = value;
-
-                Matrix4 camXform = From3x4(cameraTransform);
-
-          //      Matrix3x4 back = To3x4(camXform);
-
                 orientation.ToAxisAngle(out Vector3 axis, out float angle);
-
-                camXform = Matrix4.Mult(camXform, Matrix4.CreateFromAxisAngle(axis, -angle));
-
-
-
-                cameraNode.SetTransform(To3x4(camXform));
-
-
-
-           //     cameraNode.SetTransform()
-
-           //     Matrix3x4 cameraMatrix = cameraNode.Transform;
-             //   Matrix4 cameraMatrix4 = From3x4(cameraMatrix);
-
-
-             //   Matrix4 matx = new Matrix4(cameraNode.Transform
-
-
-       //         cameraNode.Rotation = orientation;
-
-       //         System.Diagnostics.Debug.WriteLine("Position: {0} Direction {1}", cameraNode.Position, cameraNode.Direction);
-
+                Matrix4 rotatedCameraTransform = Matrix4.Mult(cameraTransform, 
+                                                              Matrix4.CreateFromAxisAngle(axis, -angle));
+                cameraNode.SetTransform(To3x4(rotatedCameraTransform));
             }
             get
             {
                 return orientation;
             }
         }
-
 
         protected override void Start()
         {
@@ -100,7 +59,7 @@ namespace MonkeySee
             cameraNode.SetDirection(new Vector3(0, 0, 0) - cameraNode.Position);
 
             // Save the camera transform 
-            cameraTransform = cameraNode.Transform;
+            cameraTransform = From3x4(cameraNode.Transform);
 
             // Xamarin monkey model created by Vic Wang at [http://vidavic.weebly.com](http://vidavic.weebly.com "Vidavic")
             Node monkeyNode = rootNode.CreateChild("monkeyNode");
@@ -108,7 +67,7 @@ namespace MonkeySee
             monkey.Model = ResourceCache.GetModel("monkey1.mdl");
             monkey.SetMaterial(ResourceCache.GetMaterial("Materials/phong1.xml"));
 
-            // Move it down a smidge so it centered on the origin
+            // Move it down a smidge so it's centered on the origin
             monkeyNode.Translate(new Vector3(0, -3, 0));
 
             // Set up the Viewport
@@ -117,41 +76,12 @@ namespace MonkeySee
             viewport.SetClearColor(new Color(0.88f, 0.88f, 0.88f));
         }
 
-        protected override void OnUpdate(float timeStep)
-        {
-            base.OnUpdate(timeStep);
-        }
-
-
-/*
-        static Matrix3x4 CreateLookAt(Vector3 eye, Vector3 target, Vector3 up)
-        {
-            Vector3 z = -Vector3.Normalize(eye - target);
-            Vector3 x = Vector3.Normalize(Vector3.Cross(up, z));
-            Vector3 y = Vector3.Normalize(Vector3.Cross(z, x));
-
-
-
-            float offsetX = -Vector3.Dot(x, eye);
-            float offsetY = -Vector3.Dot(y, eye);
-            float offsetZ = -Vector3.Dot(z, eye);
-
-
-            Matrix3x4 rot = new Matrix3x4(new Vector3(x.X, y.X, z.X),
-                                        new Vector3(x.Y, y.Y, z.Y),
-                                        new Vector3(x.Z, y.Z, z.Z),
-                                        new Vector3(offsetX, offsetY, offsetZ));
-
-
-
-            return rot;
-        }
-*/
-
         // Matrix conversion methods
         static Matrix3x4 To3x4(Matrix4 m)
         {
-            return new Matrix3x4(m.M11, m.M21, m.M31, m.M41, m.M12, m.M22, m.M32, m.M42, m.M13, m.M23, m.M33, m.M43);
+            return new Matrix3x4(m.M11, m.M21, m.M31, m.M41, 
+                                 m.M12, m.M22, m.M32, m.M42, 
+                                 m.M13, m.M23, m.M33, m.M43);
         }
 
         static Matrix4 From3x4(Matrix3x4 m)
